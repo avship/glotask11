365576e5d1b3d2a1c45d068f9e4059a9df0af966
avship <alexandershipilo@yandex.ru>
2022-06-22 11:07:54 +0300

    Добавлено соответствие значениям

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
-const mainBtn = document.querySelector("#btn");
-const mainInput = document.querySelector('input[type="text"]');
-const squareFigure = document.querySelector("#square");
-const btn2 = document.querySelector("#e_btn");
-const inputRange = document.querySelector('input[type="range"');
-const rangeSpan = document.querySelector("#range-span");
-const circle = document.querySelector("#circle");
-mainBtn.addEventListener("click", function (e) {
-  //Если что-то введено, то кидаю это в style="backgroundColor: "
-  if (mainInput.value !== "") {
-    squareFigure.style.backgroundColor = mainInput.value;
-  }
-});
-//жмяк прячу
-btn2.style.display = "none";
-//Событие на range
-inputRange.addEventListener("change", function (e) {
+document.addEventListener("DOMContentLoaded", function () {
+  const mainBtn = document.querySelector("#btn");
+  const mainInput = document.querySelector('input[type="text"]');
+  const squareFigure = document.querySelector("#square");
+  const btn2 = document.querySelector("#e_btn");
+  const inputRange = document.querySelector('input[type="range"');
+  const rangeSpan = document.querySelector("#range-span");
   rangeSpan.textContent = inputRange.value;
-  circle.style.width = `${inputRange.value}%`;
-  circle.style.height = `${inputRange.value}%`;
+  const circle = document.querySelector("#circle");
+  mainBtn.addEventListener("click", function (e) {
+    //Если что-то введено, то кидаю это в style="backgroundColor: "
+    if (mainInput.value !== "") {
+      squareFigure.style.backgroundColor = mainInput.value;
+    }
+  });
+  //жмяк прячу
+  btn2.style.display = "none";
+  //Событие на range
+  inputRange.addEventListener("change", function (e) {
+    rangeSpan.textContent = inputRange.value;
+    circle.style.width = `${inputRange.value}%`;
+    circle.style.height = `${inputRange.value}%`;
+  });
 });
