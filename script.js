7d88070efb82d35e71d5ce877a975b3fd9eaf31c
avship <alexandershipilo@yandex.ru>
2022-06-22 12:45:25 +0300

    1)Добавлена функция updateData 2)Поменял в range событие на input 3) Добавил в span проценты"

--- script.js
+++ script.js
@@ -5,9 +5,15 @@ document.addEventListener("DOMContentLoaded", function () {
   const btn2 = document.querySelector("#e_btn");
   const inputRange = document.querySelector('input[type="range"');
   const rangeSpan = document.querySelector("#range-span");
-  rangeSpan.textContent = inputRange.value;
   const circle = document.querySelector("#circle");
-  mainBtn.addEventListener("click", function (e) {
+
+  function updateData() {
+    rangeSpan.textContent = `${inputRange.value}%`;
+    circle.style.width = `${inputRange.value}%`;
+    circle.style.height = `${inputRange.value}%`;
+  }
+  updateData();
+  mainBtn.addEventListener("mouseup", function (e) {
     //Если что-то введено, то кидаю это в style="backgroundColor: "
     if (mainInput.value !== "") {
       squareFigure.style.backgroundColor = mainInput.value;
@@ -16,9 +22,5 @@ document.addEventListener("DOMContentLoaded", function () {
   //жмяк прячу
   btn2.style.display = "none";
   //Событие на range
-  inputRange.addEventListener("change", function (e) {
-    rangeSpan.textContent = inputRange.value;
-    circle.style.width = `${inputRange.value}%`;
-    circle.style.height = `${inputRange.value}%`;
-  });
+  inputRange.addEventListener("input", updateData);
 });
